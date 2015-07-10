6b8931520dd0775e694c3c79f0c960e03816a66a
unknown <dan.dh.lee@gmail.com>
2015-07-09 18:56:56 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+var getFromApi = function(endpoint, args) {
+	var emitter = new events.EventEmitter();
+	unirest.get('https://api.spotify.com/v1/' + endpoint)
+		.qs(args).end(function(response) {
+			emitter.emit('end', response.body);
+		});
+	return emitter;
+};
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+  var searchReq = getFromApi('search', {
+    q: req.params.name,
+    limit: 1,
+    type: 'artist'
+  });
+
+  searchReq.on('end', function(item) {
+    var artist = item.artists.items[0];
+    var id = artist.id;
+    // res.json(artist);
+    
+    var relatedReq = getFromApi('artists/'+id+'/related-artists');
+    relatedReq.on('end', function(item) {
+    	artist.related = item.artists;
+    	res.json(artist);
+    });
+
+    relatedReq.on('error', function() {
+    	res.sendStatus(404);
+    });
+  });
+
+
+  searchReq.on('error', function() {
+    res.sendStatus(404);
+  });
+});
+
+app.listen(8080);
